87b12a8c7b8c57805bffe1ada2da7d463c935065
Codyane <codyane7@gmail.com>
2024-08-22 16:33:02 +0530

    Add files via upload

---
+++ variablescope.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int myNum = 3; //global variable
+
+void printNum();
+
+int main()
+{
+    //Local variable = declared inside a fucnction or block {}
+    //Global variable = declared outside of all functions 
+
+    int myNum = 1; //Local variable
+
+}
+
+void printNum(){
+
+}
