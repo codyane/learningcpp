d69aa13fbae0db0044d2f50feafb4ff75c7055ef
Codyane <codyane7@gmail.com>
2024-08-29 22:38:50 +0530

    Update memoryaddress.cpp

--- memoryaddress.cpp
+++ memoryaddress.cpp
@@ -3,13 +3,11 @@
 int main()
 {
 
-std::string name "Bro";
 bool student = true;
 int age 21;
 
-std::cout << &name << "\n';
 std::cout << &age << '\n';
-std::cout << &student << "\n";
+std::cout << &student << '\n';
 
  return 0;
 }
